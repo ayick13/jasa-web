eafecb67da639105e4c6c41c114b2332cda3cdbb
ayick13 <ayicktigabelas@gmail.com>
2025-07-17 07:52:14 +0700

    update button facebook login

--- app/components/SocialLoginButtons.tsx
+++ app/components/SocialLoginButtons.tsx
@@ -37,18 +37,14 @@ export const SocialLoginButtons = () => {
       >
         <Image src="/icons/github-icon.svg" alt="GitHub icon" width={24} height={24} />
       </button>
-       {/* --- MULAI NONAKTIFKAN DARI SINI --- */}
-      {/*
-      <button
+       <button
         onClick={() => socialLogin('facebook')}
         disabled={!!isLoading}
         className="flex items-center justify-center w-12 h-12 bg-slate-200 dark:bg-slate-700 rounded-full hover:bg-slate-300 dark:hover:bg-slate-600 transition-colors disabled:opacity-50"
         title="Masuk dengan Facebook"
       >
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor"><path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm3 8h-1.35c-.538 0-.65.221-.65.778v1.222h2l-.209 2h-1.791v7h-3v-7h-2v-2h2v-2.131c0-1.758 1.278-2.869 2.999-2.869h1.501v2z"/></svg>
+        <Image src="/icons/facebook-icon.svg" alt="facebook icon" width={24} height={24} />
       </button>
-      */}
-      {/* --- SELESAI NONAKTIFKAN DI SINI --- */}
     </div>
   );
 };
